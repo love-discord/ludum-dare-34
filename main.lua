1dc7db460ada1ff2da1eb35cfc95c45f3050be41
Sosolol261 <sosolol261@gmail.com>
2015-12-12 18:03:58 +0900

    main template

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
-function love.load()
+--[[ requirements ]]--
+	require("src.hex.hex")
 
-end
+	require("src.interactives.camera.lua")
 
 function love.update(dt)
-
+	camera:update(dt)
 end
 
 function love.draw()
+	love.graphics.push()
+	love.graphics.translate(camera.x, camera.y)
+	love.graphics.scale(camera.scale)
+	love.graphics.pop()
+end
 
+function love.mousepressed(x, y, key)
+	camera:mousepressed(key)
 end
