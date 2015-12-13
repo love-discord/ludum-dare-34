65def043a149c548393a4c9a44635e29d5eaafac
Dragomir Ioan <123ioandragomir123@gmail.com>
2015-12-13 10:36:03 +0200

    troop stuff

---
+++ src/AI/antivirus_bugfix.lua
@@ -0,0 +1,31 @@
+return function(self)	-- fighter behaviour
+	self.target = nil
+	local pT = {} -- possible targets
+
+	for _, t in pairs(immuneSystem.troop) do	-- loop through enemy troops
+		pT[#pT+1] = {x = t.x + t.xvel * t.speed, -- a little bit of prediction
+					 y = t.y + t.yvel * t.speed}
+		pT[#pT].o = t --the actual object
+	end
+
+	for _, b in pairs(immuneSystem.unit) do	-- loop through enemy buildings
+		pT[#pT + 1] = {}
+		pT[#pT].x, pT[#pT].y = hexMap:hexToPixel(b.x, b.y, b.z)
+		pT[#pT].o = b --the actual object
+	end
+
+	local minDist = 100000000
+	local idx
+	for _, p in pairs(pT) do -- loop through all possible targets in order to decide on one
+		local dx, dy = (self.x - p.x), (self.y - p.y)
+		local distSq = dx*dx + dy*dy
+		if distSq < minDist then -- if we have found a closer target
+			minDist = distSq
+			self.target = p
+		end
+	end
+
+	if minDist <= hexMap.cell_size*hexMap.cell_size then -- in range to attack
+		self.target.o.hp = self.target.o.hp - self.amount
+	end
+end
--- src/entities/immuneSystem.lua
+++ src/entities/immuneSystem.lua
@@ -12,6 +12,12 @@ function cellDamageBooster(x, y, z, amount)
 	end
 end
 
+function bugfixSpawner(x, y, z, amount)
+	if math.random() < amount then
+		immuneSystem:addTroop("Bugfix", hexMap:hexToPixel(x, y, z))
+	end
+end
+
 immuneSystem = {
 	unitList = {},
 	unit = {},
@@ -23,7 +29,7 @@ function immuneSystem:loadUnits()
 	local sampleImg = love.graphics.newImage("res/sample.png")
 	immuneSystem:newUnit("Cell Healer", 		50, 2, 32, 48, false, "Heals friendly cells with 5HP every tick." ,cellHealer, 			2, 50,  sampleImg, "Nothing", 			function() return true end)
 	immuneSystem:newUnit("Cell Damage Booster", 50, 2, 32, 48, false, "Boosts damage of friendly cells."		  ,cellDamageBooster, 	2, 150, sampleImg, "2 Cell Healers", 	function() return false end)
-	immuneSystem:newUnit("Smth else", 			50, 2, 32, 48, false, "Does something."							  ,function() end,	 	2, 666, sampleImg, "Test", 				function() return math.floor(os.time() % 2)==1  end)
+	immuneSystem:newUnit("Bugfixer Spawn",		50, 1, 32, 48, false, "Spawns bugfixes."						  ,bugfixSpawner,	 	1/12, 666, sampleImg, "Test", 				function() return math.floor(os.time() % 2)==1  end)
 	immuneSystem:newUnit("Smth else2", 			50, 2, 32, 48, false, "Does something different."				  ,function() end,	 	2, 666, sampleImg, "Test", 				function() return math.floor(os.time() % 2)==0  end)
 	return immuneSystem.unitList
 end
@@ -59,26 +65,46 @@ function immuneSystem:addUnit(name, x, y, z)
 	end
 end
 
+function immuneSystem:loadTroops()
+	immuneSystem:newTroop("Bugfix", 1, 0, 10, 10, 4, require("src.AI.antivirus_bugfix"), 50)
+end
+
+function immuneSystem:newTroop(name, hp, range, w, h, amount, effect, speed)
+	immuneSystem.troopList[name] = {name = name, hp = hp, range = range, w = w, h = h, amount = amount, effect = effect, speed = speed}
+end
+
+function immuneSystem:addTroop(name, x, y)
+	table.insert(self.troop,		{name = name, x = x, y = y, hp = self.troopList[name].hp,
+									range = self.troopList[name].range,
+									w = self.troopList[name].w,
+									h = self.troopList[name].h,
+									effect = self.troopList[name].effect,
+									speed = self.troopList[name].speed,
+									amount = self.troopList[name].amount,
+									target = nil, xvel = 0, yvel = 0})
+end
+
+
 function immuneSystem:remove(x, y, z)
-	local occupied, id = immuneSystem:find(x, y)
+	local occupied, id = self:find(x, y)
 	local tempUnit = {}
 
 	if occupied then
-		for i = 1, #immuneSystem.unit do
+		for i = 1, #self.unit do
 			if i ~= id then
-				tempUnit[#tempUnit + 1] = immuneSystem.unit[i]
+				tempUnit[#tempUnit + 1] = self.unit[i]
 			end
-			immuneSystem.unit[i] = nil
+			self.unit[i] = nil
 		end
 
-		immuneSystem.unit = tempUnit
+		self.unit = tempUnit
 	end
 end
 
 function immuneSystem:update(dt)
-	for i, unit in pairs(immuneSystem.unit) do
+	for i, unit in pairs(self.unit) do
 		if unit.hp <= 0 then
-			immuneSystem.unit[i] = nil
+			self.unit[i] = nil
 		else
 			local inRange = hexMap:inRange(unit.x, unit.y, unit.z, unit.range)
 			for v = 1, #inRange do
@@ -86,13 +112,35 @@ function immuneSystem:update(dt)
 			end
 		end
 	end
+	for i, t in pairs(self.troop) do
+		t:effect()
+	end
+end
+
+function immuneSystem:fastUpdate(dt)
+	for i, v in pairs(self.troop) do
+		v.xvel, v.yvel = 0, 0
+		if v.target then
+			v.xvel = v.target.x - v.x
+			v.yvel = v.target.y - v.y
+		end
+		local velM = math.sqrt(v.xvel * v.xvel + v.yvel * v.yvel)
+		if velM > hexMap.cell_size then -- if not in range (should still move)
+			v.x = v.x + v.xvel / velM * dt * v.speed
+			v.y = v.y + v.yvel / velM * dt * v.speed
+		end
+	end
 end
 
 function immuneSystem:draw()
-	for i, unit in pairs(immuneSystem.unit) do
+	for i, unit in pairs(self.unit) do
 		local x, y = hexMap:hexToPixel(unit.x, unit.y, unit.z)
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.rectangle("fill", x - unit.w / 2, y - unit.h / 2 - 10, unit.w, unit.h)
+	end
+	for i, t in pairs(self.troop) do
+		love.graphics.setColor(0, 100, 0)
+		love.graphics.rectangle("fill", t.x - t.w / 2, t.y - t.h / 2 - 10, t.w, t.h)
 	end
 end
 
