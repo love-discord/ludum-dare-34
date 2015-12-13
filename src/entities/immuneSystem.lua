31465bcdca69cfc9e7df93c3c0369b1542119fd1
Sosolol261 <sosolol261@gmail.com>
2015-12-13 17:34:05 +0900

    NO MORE CONFLICTS

--- src/entities/immuneSystem.lua
+++ src/entities/immuneSystem.lua
@@ -89,19 +89,16 @@ function immuneSystem:update(dt)
 end
 
 function immuneSystem:draw()
-<<<<<<< HEAD
 	for i = 1, #immuneSystem.unit do
 		local x, y = hexMap:hexToPixel(immuneSystem.unit[i].x, immuneSystem.unit[i].y, immuneSystem.unit[i].z)
 		local sX = hexMap.cell_size / immuneSystem.unit[i].img:getWidth() 
 		local sY = (hexMap.cell_size + hexMap.cell_size / 2) / immuneSystem.unit[i].img:getHeight()
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.draw(immuneSystem.unit[i].img, x - hexMap.cell_size / 2, y - (hexMap.cell_size + hexMap.cell_size / 2) / 2 - 10, 0, sX, sY)
-=======
 	for i, unit in pairs(immuneSystem.unit) do
 		local x, y = hexMap:hexToPixel(unit.x, unit.y, unit.z)
 		love.graphics.setColor(255, 255, 255)
 		love.graphics.rectangle("fill", x - unit.w / 2, y - unit.h / 2 - 10, unit.w, unit.h)
->>>>>>> origin/master
 	end
 end
 
