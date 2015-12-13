4ccb3a063a6f7cb78673025d469268c9a7e6111f
Dragomir Ioan <123ioandragomir123@gmail.com>
2015-12-13 11:49:19 +0200

    mouse.lua

---
+++ src/interactives/mouse.lua
@@ -0,0 +1,31 @@
+-- src/interactives/mouse.lua
+
+local mouse = {}
+
+function mouse:update()
+	self.screenX = love.mouse.getX()
+	self.screenY = love.mouse.getY()
+end
+
+function mouse:getX()
+	return self.screenX
+end
+function mouse:getY()
+	return self.screenY
+end
+
+function mouse:scaledX()
+	local sx = self.screenX
+	return sx
+end
+
+function mouse:scaledY()
+	local sy = self.screenY
+	return sy
+end
+
+function mouse:getHex()
+	return hexMap:pixelToHex(self:scaledX(), self:scaledY())
+end
+
+return mouse
